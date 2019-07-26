6b25045e799bb5dff3b668e5c27db31934795de5
Clayton Mork <mork.clayton3@gmail.com>
2019-07-26 18:05:06 +0900

    Added get_list_dimension method to utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def parse_str_from_tag(text: str, tag: str) -> str:
     return text.split(f'<{tag}>')[1].split(f'</{tag}>')[0]
 
@@ -7,3 +9,6 @@ def parse_int_from_tag(text: str, tag: str) -> int:
 def str2intlist(list_str: str) -> list:
     temp = list_str.split('[')[1].split(']')[0].replace(' ', '').split(',')
     return [int(i) for i in temp]
+
+def get_list_dimension(list_data: list) -> int:
+    return len(np.array(list_data).shape)
