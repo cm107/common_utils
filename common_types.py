f340be46e90fae2c7fb82b589cd012bafe0c67c8
Clayton Mork <mork.clayton3@gmail.com>
2019-07-31 17:35:45 +0900

    Added Keypoint and BoundingBox to common_types.py

--- common_types.py
+++ common_types.py
@@ -2,3 +2,5 @@ from collections import namedtuple
 
 Point = namedtuple('Point', ['x', 'y'])
 Size = namedtuple('Size', ['width', 'height'])
+Keypoint = namedtuple('Keypoint', ['x', 'y', 'v'])
+BoundingBox = namedtuple('BoundingBox', ['xmin', 'ymin', 'xmax', 'ymax'])
