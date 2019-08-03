65007de85aeb89e61f62eec8ef87ca0a33d2268d
Clayton Mork <mork.clayton3@gmail.com>
2019-08-04 03:54:59 +0900

    Added counter_utils.py

--- path_utils.py
+++ path_utils.py
@@ -77,6 +77,17 @@ def get_all_files_of_extension(dir_path: str, extension: str=None) -> list:
         filepaths = filepaths_of_extension
     return filepaths
 
+def get_all_files_in_extension_list(dir_path: str, extension_list: list=None) -> list:
+    filepaths = get_pathlist(dir_path)
+    if extension_list is not None:
+        filepaths_of_extension = []
+        for filepath in filepaths:
+            ext = get_extension_from_path(filepath)
+            if ext in extension_list:
+                filepaths_of_extension.append(filepath)
+        filepaths = filepaths_of_extension
+    return filepaths
+
 def file_extension_exists_in_dir(dir_path: str, extension: str) -> bool:
     ext_filepaths = get_all_files_of_extension(dir_path, extension)
     if len(ext_filepaths) > 0:
