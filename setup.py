b92222e879d402856907282aae1852f2effdf0c6
Clayton Mork <mork.clayton3@gmail.com>
2019-10-23 11:21:36 +0900

    Still trying to fix logger requirement

--- setup.py
+++ setup.py
@@ -30,10 +30,7 @@ setup(
         'requests>=2.22.0',
         'pylint>=2.4.2',
         'tqdm>=4.36.1',
-        'logger==0.1'
-    ],
-    dependency_links=[
-        'git+https://github.com/cm107/logger#egg=logger-0.1',
+        'logger>=0.1@https://github.com/cm107/logger#egg=logger'
     ],
     python_requires='>=3.6'
 )
