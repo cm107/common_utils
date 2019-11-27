c560df4b0964a05efb7762e90101545bee9f0684
Clayton Mork <mork.clayton3@gmail.com>
2019-11-27 15:49:57 +0900

    Added ResumeWeightsChecker to ml_utils/resume

--- common_utils/prompt_utils/prompt_utils.py
+++ common_utils/prompt_utils/prompt_utils.py
@@ -3,24 +3,37 @@ from logger import logger
 from ..adv_file_utils import delete_all_files_in_extension_list
 from ..file_utils import delete_dir
 
-def delete_files_in_dir_prompt(dir_path: str, extension_list: list):
-    answer = input("yes/no: ")
-    if answer.lower() == "yes":
-        delete_all_files_in_extension_list(dir_path=dir_path, extension_list=extension_list)
-    elif answer.lower() == "no":
-        logger.warning("Terminating program.")
-        sys.exit()
+def standard_prompt() -> bool:
+    answer = input('yes/no: ')
+    if answer.lower() in ['y', 'yes']:
+        consent = True
+    elif answer.lower() in ['n', 'no']:
+        consent = False
     else:
         logger.error(f"Invalid response: {answer}")
         raise Exception
+    return consent
+
+def terminate_on_no_prompt() -> bool:
+    if standard_prompt():
+        return True
+    else:
+        logger.warning("Terminating program.")
+        sys.exit()
+
+def delete_files_in_dir_prompt(dir_path: str, extension_list: list):
+    if terminate_on_no_prompt():
+        delete_all_files_in_extension_list(dir_path=dir_path, extension_list=extension_list)
 
 def delete_dir_prompt(dir_path: str):
-    answer = input("yes/no: ")
-    if answer.lower() == "yes":
+    if terminate_on_no_prompt():
         delete_dir(dir_path=dir_path)
-    elif answer.lower() == "no":
-        logger.warning("Terminating program.")
-        sys.exit()
-    else:
-        logger.error(f"Invalid response: {answer}")
-        raise Exception
+
+def weights_scratch_prompt() -> bool:
+    logger.warning(f"Would you like to start from scratch?")
+    if terminate_on_no_prompt():
+        return True
+
+def weights_newest_prompt(newest_weights_path: str) -> bool:
+    logger.warning(f"Would you like to resume from the newest weights path?\n{newest_weights_path}")
+    return standard_prompt()
