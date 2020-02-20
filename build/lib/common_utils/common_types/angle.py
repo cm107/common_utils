27a070ba3ae602ae4c35ccc99be08a0b19ba8982
Clayton Mork <mork.clayton3@gmail.com>
2020-02-20 12:08:46 +0900

    Updated EulerAngle and Quaternion class conversions.

--- build/lib/common_utils/common_types/angle.py
+++ build/lib/common_utils/common_types/angle.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import math
 from common_utils.check_utils import check_type_from_list
 from common_utils.common_types.constants import number_types
+from scipy.spatial.transform import Rotation
 
 class EulerAngle:
     def __init__(self, roll, pitch, yaw):
@@ -20,24 +21,15 @@ class EulerAngle:
         return [self.roll, self.pitch, self.yaw]
 
     @classmethod
-    def from_list(self, val_list: list) -> EulerAngle:
+    def from_list(self, val_list: list, from_deg: bool=False) -> EulerAngle:
         roll, pitch, yaw = val_list
-        return EulerAngle(roll=roll, pitch=pitch, yaw=yaw)
+        if from_deg:
+            return EulerAngle(roll=roll*math.pi/180, pitch=pitch*math.pi/180, yaw=yaw*math.pi/180)
+        else:
+            return EulerAngle(roll=roll, pitch=pitch, yaw=yaw)
 
-    def to_quaternion(self) -> Quaternion:
-        cy = math.cos(self.yaw * 0.5)
-        sy = math.sin(self.yaw * 0.5)
-        cp = math.cos(self.pitch * 0.5)
-        sp = math.sin(self.pitch * 0.5)
-        cr = math.cos(self.roll * 0.5)
-        sr = math.sin(self.roll * 0.5)
-
-        qw = cy * cp * cr + sy * sp * sr
-        qx = cy * cp * sr - sy * sp * cr
-        qy = sy * cp * sr + cy * sp * cr
-        qz = sy * cp * cr - cy * sp * sr
-
-        return Quaternion(qw=qw, qx=qx, qy=qy, qz=qz)
+    def to_quaternion(self, seq: str='xyz') -> Quaternion:
+        return Quaternion.from_list(Rotation.from_euler(seq=seq, angles=self.to_list()).as_quat().tolist())
 
     def to_deg_list(self) -> list:
         return [val * 180 / math.pi for val in self.to_list()]
@@ -64,22 +56,5 @@ class Quaternion:
         qw, qx, qy, qz = val_list
         return Quaternion(qw=qw, qx=qx, qy=qy, qz=qz)
 
-    def to_euler(self) -> EulerAngle:
-        # roll
-        sinr_cosp = 2 * (self.qw * self.qx + self.qy * self.qz)
-        cosr_cosp = 1 - 2 * (self.qx * self.qx + self.qy * self.qy)
-        roll = math.atan2(sinr_cosp, cosr_cosp)
-
-        # pitch
-        sinp = 2 * (self.qw * self.qy - self.qz * self.qx)
-        if abs(sinp) >= 1:
-            pitch = math.copysign(math.pi / 2, sinp) # 90deg when out of range
-        else:
-            pitch = math.asin(sinp)
-
-        # yaw
-        siny_cosp = 2 * (self.qw * self.qz + self.qx * self.qy)
-        cosy_cosp = 1 - 2 * (self.qy * self.qy + self.qz * self.qz)
-        yaw = math.atan2(siny_cosp, cosy_cosp)
-
-        return EulerAngle(roll=roll, pitch=pitch, yaw=yaw)
+    def to_euler(self, seq: str='xyz') -> EulerAngle:
+        return EulerAngle.from_list(Rotation.from_quat(self.to_list()).as_euler(seq=seq).tolist())
